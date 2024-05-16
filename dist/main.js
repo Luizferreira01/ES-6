3c8715fdd1cb65c2c4aab244f2fd7d061150fa78
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-05-15 21:20:05 -0300

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,25 @@
 "use strict";
 
-console.log('ola mundo');
+var alunos = [{
+  nome: "Maria",
+  nota: 8
+}, {
+  nome: "João",
+  nota: 5
+}, {
+  nome: "Pedro",
+  nota: 7
+}, {
+  nome: "Ana",
+  nota: 4
+}, {
+  nome: "Carla",
+  nota: 9
+}];
+function alunosComNotaMinima(listaAlunos, notaMinima) {
+  return listaAlunos.filter(function (aluno) {
+    return aluno.nota >= notaMinima;
+  });
+}
+var alunosAprovados = alunosComNotaMinima(alunos, 6);
+console.log(alunosAprovados);
