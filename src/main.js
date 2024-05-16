564cfa438fe05bebdaa3cbe6c1a072bd72016897
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-05-15 21:20:09 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-console.log('ola mundo')
+const alunos = [
+    { nome: "Maria", nota: 8 },
+    { nome: "João", nota: 5 },
+    { nome: "Pedro", nota: 7 },
+    { nome: "Ana", nota: 4 },
+    { nome: "Carla", nota: 9 },
+];
+
+function alunosComNotaMinima(listaAlunos, notaMinima) {
+    return listaAlunos.filter(aluno => aluno.nota >= notaMinima);
+}
+
+const alunosAprovados = alunosComNotaMinima(alunos, 6);
+console.log(alunosAprovados);
